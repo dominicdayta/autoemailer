422d981e9d75f1ee645e8eebcb13bdb22ab9f756
dominicdayta <dbdayta@up.edu.ph>
2021-10-28 09:39:44 +0800

    Create trial message

--- emailer.py
+++ emailer.py
@@ -9,13 +9,13 @@ sender_email = os.getenv("SENDER_EMAIL")
 password = os.getenv("SMTP_PASSWORD")
 
 
-receiver_email = "your@gmail.com"
+receiver_email = "dbdayta@up.edu.ph"
 message = """\
 Subject: Hi there
 
 This message is sent from Python."""
 
-print(port)
-print(smtp_server)
-print(password)
-print(sender_email)
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
